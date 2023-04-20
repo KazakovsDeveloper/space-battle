6f84fff320b27aadd59af3f0ee52f875e8ad01b7
Maxim Kazakov <m.c.kazakov.developer@gmail.com>
2023-04-20 14:50:56 +0300

    add exist commands

---
+++ src/main/java/ru/otus/space/battle/MoveCommand.java
@@ -0,0 +1,9 @@
+package ru.otus.space.battle;
+
+public class MoveCommand {
+
+    public boolean execute() {
+        return false;
+    }
+
+}
---
+++ src/main/java/ru/otus/space/battle/RotateCommand.java
@@ -0,0 +1,9 @@
+package ru.otus.space.battle;
+
+public class RotateCommand {
+
+    public boolean execute() {
+        return false;
+    }
+
+}
