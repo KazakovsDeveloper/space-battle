88710a9f8394359026061386334e0359ba785321
Maxim Kazakov <m.c.kazakov.developer@gmail.com>
2023-05-23 09:33:55 +0300

    Add threading to MacroCommand

--- src/main/java/ru/otus/space/battle/command/MacroCommand.java
+++ src/main/java/ru/otus/space/battle/command/MacroCommand.java
@@ -1,25 +1,32 @@
 package ru.otus.space.battle.command;
 
-import ru.otus.space.battle.exception.CommandException;
+import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class MacroCommand {
+    private final ConcurrentLinkedQueue<Command> commands;
+    private final ExecutorService executor;
 
-    private final Command[] commands;
-
-    public MacroCommand(Command[] commands) {
+    public MacroCommand(ConcurrentLinkedQueue<Command> commands) {
         this.commands = commands;
+        this.executor = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
     }
 
     public void execute() {
-        for (Command command : commands) {
-
-            try {
-                command.execute();
-            } catch (RuntimeException exception) {
-                throw new CommandException("Что-то пошло не так " + exception);
+        while (!commands.isEmpty()) {
+            Command command = commands.poll();
+            if (command != null) {
+                executor.submit(() -> {
+                    try {
+                        System.out.println(command.getClass() + " ");
+                        command.execute();
+                    } catch (RuntimeException exception) {
+                        System.out.println("Исключение: " + exception);
+                    }
+                });
             }
-
         }
+        executor.shutdown();
     }
-
 }
