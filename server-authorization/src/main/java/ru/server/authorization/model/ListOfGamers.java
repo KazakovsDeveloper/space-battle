f8180f7c54fb2cee4e51e0b0bc4debc74f5e1351
Maxim Kazakov <m.c.kazakov.developer@gmail.com>
2023-06-08 15:13:25 +0300

    Add main structure of auth-server

---
+++ server-authorization/src/main/java/ru/server/authorization/model/ListOfGamers.java
@@ -0,0 +1,29 @@
+package ru.server.authorization.model;
+
+import jakarta.validation.constraints.NotNull;
+
+import java.util.List;
+
+public class ListOfGamers {
+
+    @NotNull(message = "Организатор игры должен быть указан")
+    private Gamer managerOfGame;
+
+    private List<Gamer> gamers;
+
+    public ListOfGamers(Gamer managerOfGame, List<Gamer> gamers) {
+        this.managerOfGame = managerOfGame;
+        this.gamers = gamers;
+    }
+
+    public ListOfGamers() {
+    }
+
+    public List<Gamer> getGamers() {
+        return gamers;
+    }
+
+    public Gamer getManagerOfGame() {
+        return managerOfGame;
+    }
+}
