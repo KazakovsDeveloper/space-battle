92d15525496533f63b762adc01a74e46abcfaa82
Maxim Kazakov <m.c.kazakov.developer@gmail.com>
2023-06-08 16:25:20 +0300

    Add tests for token controller

---
+++ server-authorization/src/main/java/ru/server/authorization/controller/TokenController.java
@@ -0,0 +1,28 @@
+package ru.server.authorization.controller;
+
+import jakarta.validation.constraints.NotNull;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RestController;
+import ru.server.authorization.service.TokenService;
+
+@RestController
+public class TokenController {
+
+    private final TokenService tokenService;
+
+    public TokenController(TokenService tokenService) {
+        this.tokenService = tokenService;
+    }
+
+    @PostMapping("/createToken")
+    public ResponseEntity<String> generateToken(@RequestBody String tankBattleId,
+                                                @RequestHeader
+                                                @NotNull String gamerLogin) {
+        String token = tokenService.generateToken(tankBattleId, gamerLogin);
+        return ResponseEntity.ok(token);
+    }
+
+}
